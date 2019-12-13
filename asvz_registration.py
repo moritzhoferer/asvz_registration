d9f1891a0edc69db7c6f2521b1584bb4f26eb2ef
45635700 <moritzhoferer@gmail.com>
2019-12-13 21:49:44 +0100

    Check credentials for correctness at begin

--- asvz_registration.py
+++ asvz_registration.py
@@ -9,6 +9,13 @@ import dateutil.parser
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
+
+
+def open_Firefox(headless=True):
+    driver_options = Options()
+    if headless: driver_options.add_argument('-headless')
+    return webdriver.Firefox(executable_path='geckodriver', options=driver_options)
 
 
 def get_credentials() -> list:
@@ -90,6 +97,7 @@ def filter_sportfahrplan(_df, _filter) -> pd.DataFrame:
 
 
 def get_lesson_info(s: pd.Series):
+    # TODO Bring this output in a nice format!
     print(s.sport_name)
     print(s.to_date)
     print(s.instructor_name)
@@ -112,10 +120,41 @@ def load_preferences(file_name: str):
     return dict_
 
 
-if __name__ == '__main__':
-    # Enter username and password of the user to register
-    # TODO Check for correctness of the credentials
-    usr, pwd = get_credentials()
+if __name__ == '__main__':    
+    driver = open_Firefox()
+    driver.get('https://auth.asvz.ch/account/login')
+    switchAai_button = driver.find_element_by_xpath('//*[@title="SwitchAai Account Login"]') 
+    switchAai_button.click()
+
+    # Skip if the selection of saved in the browser's chache
+    if driver.current_url.startswith('https://wayf.switch.ch/'):
+        # Select institution to get to ETH Login
+        input_box = driver.find_element_by_id('userIdPSelection_iddtext')
+        input_box.send_keys('ETH Zurich')
+        send_button = driver.find_element_by_name('Select')
+        send_button.click()
+    
+    while True:
+        # Enter username and password of the user to register
+        usr, pwd = get_credentials()
+        
+        # Enter credentials
+        user_box = driver.find_element_by_id('username')
+        user_box.clear()
+        user_box.send_keys(usr)
+        password_box = driver.find_element_by_id('password')
+        password_box.send_keys(pwd)
+        login_button = driver.find_element_by_id('LoginButtonText')
+        login_button.click()
+
+        try:
+            driver.find_element_by_xpath('//*[@class="validation_false validation_info"]')
+            print('Authentication failed! Username, password or both are wrong.')
+        except NoSuchElementException:
+            print('Authentication successfully checked!')
+            driver.quit()
+            break
+
     # Preferred lesson: Dictionary with filters
     if len(sys.argv) > 1:
         preferred_lesson = load_preferences(sys.argv[1])
@@ -124,7 +163,8 @@ if __name__ == '__main__':
 
     # Load the lesson schedule "Sportfahrplan" and filter of the desired lesson
     sportfahrplan = get_sportfahrplan(filter=preferred_lesson)
-    # find the next lesson you want to register for
+    # find the next lesson with free places you want to register for
+    sportfahrplan = sportfahrplan[sportfahrplan.places_free > 0]
     next_lesson = sportfahrplan.iloc[0]
     get_lesson_info(next_lesson)
     registration_time = next_lesson.oe_from_date.astimezone(tz=datetime.timezone.utc)
@@ -167,7 +207,7 @@ if __name__ == '__main__':
         login_button = driver.find_element_by_id('LoginButtonText')
         login_button.click()
 
-    # print('The login took approx. {sec:.2f} seconds.'.format(sec=delta_time.total_seconds()))
+    # Wait until the registration is opened
     waiting_period = get_time_until(registration_time) + 2
     if waiting_period > 0:
         print('Wait for {sec:.1f} minutes until registration'.format(sec=waiting_period))
@@ -175,5 +215,5 @@ if __name__ == '__main__':
     # Finally, register for the lesson
     lesson_login_button = driver.find_element_by_id('btnRegister')
     lesson_login_button.click()
-    # # Quit browser
+    # Quit browser
     driver.quit()
